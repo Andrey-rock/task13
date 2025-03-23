77710afacf164a7ac73c5aa858e45de9509f4326
Andrey-rock <andrey-rok@mail.ru>
2025-03-23 16:14:12 +0300

    Задача к билету 13 решена. Изм. для PR

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 public class Main {
     public static void main(String[] args) {
         Collection<User> users = new ArrayList<>();
+
         users.add(new User("Иван", 38));
         users.add(new User("Петр", 38));
         users.add(new User("Мария", 21));
